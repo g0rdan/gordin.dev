7e158e640292468cd4d86b8c0913702e90b45990
Denis Gordin <gordin.dan@gmail.com>
2023-05-02 21:41:12 -0400

    chore: update_flutter_and_dependencies

--- gordindev/lib/main.dart
+++ gordindev/lib/main.dart
@@ -8,8 +8,9 @@
  */
 
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:gordindev/routing/app_router.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -21,7 +22,7 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
+  usePathUrlStrategy();
   runApp(const MyApp());
 }
 
